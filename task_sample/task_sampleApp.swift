14564b9198b82381b32c0d9e0ed5cb25cc0db26d
tomita-anima <94331796+tomita-anima@users.noreply.github.com>
2021-12-30 01:06:03 +0900

    Initial Commit

---
+++ task_sample/task_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_sampleApp.swift
+//  task_sample
+//
+//  Created by 冨田卓考 on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct task_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
